263fb2b1ecdaf6a1524186a4669f1ef83078af2d
unknown <Luis@DESKTOP-V9OG3NJ.ap.serv>
2021-12-08 20:56:43 -0300

    Segunda Versão do arquivo integrado com github

--- Script_integrado_git.R
+++ Script_integrado_git.R
@@ -2,3 +2,7 @@
 # Exercício de utilização do "R" integrado com Github
 # Primeira Versâo do arquivo de teste.
 library("tydverse")
+# Novas linhas de comando
+# Segund etapa
+library(readxl)
+# Fim da Segunda parte
