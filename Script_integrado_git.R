130a66c889d72584dae1915e0d75221f2448a47a
Luis Silva <94875522+claudioluis-silva@users.noreply.github.com>
2021-12-08 21:06:15 -0300

    Terceira Versao do Projeto

--- Script_integrado_git.R
+++ Script_integrado_git.R
@@ -6,3 +6,6 @@ library("tydverse")
 # Segunda etapa
 library(readxl)
 # Fim da Segunda parte
+#Verão 3
+# Alterações remotas no github
+dataset_inicial <- read_excel("(1.2) Dataset Aula Data Wrangling.xls")
